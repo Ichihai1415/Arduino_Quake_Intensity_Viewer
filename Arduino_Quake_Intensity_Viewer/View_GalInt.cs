d6ded5f5d3fe61ea55bf127f07cec9abcd64e7d6
Ichihai1415 <projects31415.gif@gmail.com>
2023-04-21 18:25:13 +0900

    Z軸表示がX軸のものになっていた問題を修正

--- Arduino_Quake_Intensity_Viewer/View_GalInt.cs
+++ Arduino_Quake_Intensity_Viewer/View_GalInt.cs
@@ -14,7 +14,7 @@ namespace Arduino_Quake_Intensity_Viewer
 
         private void View_Tick(object sender, EventArgs e)
         {
-            MainText.Text = $"{string.Format("{0:0000.00}", GalNow[0])}gal\n{string.Format("{0:0000.00}", GalNow[1])}gal\n{string.Format("{0:0000.00}", GalNow[0])}gal\n合成加速度:{string.Format("{0:0000.00}", GalNow[3])}gal\n合成加速度最大:{string.Format("{0:0000.00}", GalMax)}gal\n震度:{string.Format("{0:0.00}", IntNow)}".Replace("震度:0.00", "震度:---");
+            MainText.Text = $"{string.Format("{0:0000.00}", GalNow[0])}gal\n{string.Format("{0:0000.00}", GalNow[1])}gal\n{string.Format("{0:0000.00}", GalNow[2])}gal\n合成加速度:{string.Format("{0:0000.00}", GalNow[3])}gal\n合成加速度最大:{string.Format("{0:0000.00}", GalMax)}gal\n震度:{string.Format("{0:0.00}", IntNow)}".Replace("震度:0.00", "震度:---");
             if (GalNow[0] == 0.001)
                 MainText.Text = $"加速度X:0000.00gal\n加速度Y:0000.00gal\n加速度Z:0000.00gal\n合成加速度:0000.00gal\n合成加速度最大:0000.00gal\n震度:0.00";
             if (GalMax < 15)
